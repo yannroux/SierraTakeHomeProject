380ce502e86722c7d07c85ff0384759ebb7a3012
yann roux <yann.roux@improving.com>
2023-07-18 16:50:46 -0600

    Add project files.

---
+++ OrderManagementSystem/OrderManagementSystem.Core/Models/Order.cs
@@ -0,0 +1,10 @@
+﻿namespace OrderManagementSystem.Core.Models;
+
+public class Order
+{
+    public int Id { get; set; }
+    public int CustomerId { get; set; }
+    public int ProductId { get; set; }
+    public double Quantity { get; set; }
+    public double TotalCost { get; set; }
+}
---
+++ OrderManagementSystem/OrderManagementSystem.DataAccess/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace OrderManagementSystem.DataAccess
+{
+    public class Class1
+    {
+
+    }
+}
